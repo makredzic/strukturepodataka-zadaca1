46e268964345fd3a434d0cc41058057f31e07f36
makredzic <mak.redzic@gmail.com>
2021-04-21 19:34:32 +0200

    Dodane brojne izmjene, zavrseni metodi svi i komplet dvostruko-povezana lista.

--- zad1/lista.hpp
+++ zad1/lista.hpp
@@ -1,11 +1,12 @@
 #pragma once
-#include <iostream>
-#include <utility>
 #include <cstddef> // size_t --> type_traits ukljucuje cstddef
+#include <exception>
+#include <iostream>
 #include <iterator>
+#include <stdexcept>
+#include <utility>
 
-template <typename T>
-class List {
+template <typename T> class List {
 
 private:
   struct Node {
@@ -13,128 +14,130 @@ private:
     Node* next_ = nullptr;
     Node* prev_ = nullptr;
 
-    Node(const T& el, Node* n = nullptr, Node* p = nullptr) : element_{el}, next_{n}, prev_{p} {} 
-    Node(T&& el, Node* n = nullptr, Node* p = nullptr) : element_{std::move(el)}, next_{n}, prev_{p} {}
+    Node(const T &el, Node *n = nullptr, Node *p = nullptr)
+        : element_{el}, next_{n}, prev_{p} {}
+    Node(T &&el, Node *n = nullptr, Node *p = nullptr)
+        : element_{std::move(el)}, next_{n}, prev_{p} {}
   };
 
-  Node* head_{nullptr}; //pokazivac na 1. node
-  Node* tail_{nullptr}; //pokazivac na zadnji node
+  Node* head_{nullptr}; // pokazivac na 1. node
+  Node* tail_{nullptr}; // pokazivac na zadnji node
   size_t size_{0};
 
 public:
   // Pravilo 6-torke
   List() = default; // ne zahtjeva implementaciju jer su clanovi klase default konstruisani
 
-  List(const List& other);
-  List(List&&);
-  List& operator=(const List&);
-  List& operator=(List&&);
-  ~List() {clear();};
-  
-  size_t size() const {return size_;}
-  bool empty() const { return size_ == 0;}
+  List(const List &other);
+  List(List &&);
+  List &operator=(const List &);
+  List &operator=(List &&);
+  ~List() { clear(); };
+
+  size_t size() const { return size_; }
+  bool empty() const { return size_ == 0; }
 
   void clear();
 
-  void push_back(const T&);
-  void push_back(T&&);
+  void push_back(const T &);
+  void push_back(T &&);
 
-  void push_front(const T&);
-  void push_front(T&&);
+  void push_front(const T &);
+  void push_front(T &&);
 
   void pop_back();
   void pop_front();
 
   void print() const;
 
-  class iterator : public std::iterator<std::bidirectional_iterator_tag, T>{
-    private:
-      Node* p_{nullptr};
-    public:
-      iterator() = default;
-      iterator(Node* ptr) : p_{ptr} {}
-			iterator& operator=(const iterator& other) {
-				p_ = other.p_;
-				return *this;
+  class iterator : public std::iterator<std::bidirectional_iterator_tag, T> {
+  private:
+    Node* p_;
+		Node* pPrev_;
+
+  public:
+    iterator() = delete;
+
+    iterator(Node* ptrCurrent = nullptr, Node* ptrPrev = nullptr) : p_{ptrCurrent}, pPrev_{ptrPrev} {
+			if (!ptrCurrent && !ptrPrev) throw std::invalid_argument("Both arguments can't be nullptr.");
+		}
+
+		iterator(const iterator& other) : p_{other.p_}, pPrev_{other.pPrev_} {} //copy-constructor
+
+
+    iterator &operator=(const iterator &other) {
+      p_ = other.p_;
+			pPrev_ = other.pPrev_;
+      return *this;
+    }
+
+    iterator &operator++() {
+			if (p_ != nullptr) { //u slucaju da trenutni p_ nije nullptr odnosno nije end()
+				pPrev_ = p_;
+				p_ = p_->next_;
 			}
 
-      iterator& operator++() {
-        if (p_ != nullptr) p_ = p_->next_;  
-        return *this;
-      };
-      
-      iterator operator++(int) {
-        auto temp = iterator{p_};
-        if (p_ != nullptr) p_ = p_->next_;
-        
-        return temp;
-      };
-      
-      iterator& operator--() {
-        if (p_ == nullptr) p_ = tail_; else
-				if (p_ != head_) p_ = p_->prev_; 
-        return *this;
-      };
+      return *this;
+    };
 
-      
-      iterator operator--(int) {
-        auto temp = iterator{p_};
-        if (p_ == nullptr) p_ = tail_; else
-				if (p_ != head_) p_ = p_->prev_; 
-        
-        return temp;
-      };
+    iterator operator++(int) {
+      auto temp = iterator{p_};
+			if (p_ != nullptr) {
+				pPrev_ = p_;
+				p_ = p_->next_;
+			}
 
-      bool operator==(const iterator& other) const {
-        return p_ == other.p_;
-      }
-      
-      bool operator!=(const iterator& other) const {
-        return p_ != other.p_;
-      }
-      
-      T& operator*() {
-        return p_->element_;
-      };
-      
-      T& operator->() {
-        return p_->element_; 
-      }
+      return temp;
+    };
 
-			friend List;
-	};
+    iterator& operator--() {
+			if (pPrev_ != nullptr) {
+				p_ = pPrev_;
+				pPrev_ = pPrev_ -> prev_;
+			}
 
-  iterator begin() {return iterator{head_};}
-  iterator end() {return iterator{};} //vracamo iterator koji pokazuje na nullptr, default konstruktor inicijalizira sa nullptr pa ne pise u {} eskplicitno nullptr
+      return *this;
+    };
 
-  void insert(iterator&, const T&);
-  void remove(iterator);
-  
+    iterator operator--(int) {
+      auto temp = iterator{p_};
+			if (pPrev_ != nullptr) {
+				p_ = pPrev_;
+				pPrev_ = pPrev_ -> prev_;
+			}
+
+      return temp;
+    };
+
+    bool operator==(const iterator &other) const { return p_ == other.p_; } //nema potrebe porediti i prethodne pointere jer se uvijek zajendo krecu
+    bool operator!=(const iterator &other) const { return p_ != other.p_; }
+
+    T& operator*() { 
+			if (p_ == nullptr) throw std::string("Error calling operator*(). Can't dereference nullptr.\n"); //trebaju se samo exceptions bacati, nisam mogao naci koji std excpetion odgovara ovome zato sam odlucio bolje string exception baciti nego dozvoliti pokusaj dereferenciranja nullptr
+			return p_->element_;
+		}
+    T& operator->() {
+			if (p_ == nullptr) throw std::string("Error calling operator->(). Can't dereference nullptr.\n"); 
+			return p_->element_;
+		}
+
+    friend List;
+  };
+
+  iterator begin() { return iterator{head_}; }
+  iterator end() {return iterator{nullptr, tail_}; } //konstruktor od iteratora uzima 2 argumenta, 1. je pointer na trenutni, 2. je pointer na prethodni
+
+	//potreban je overload sa rvalue referencom za slucaj da proslijedimo it++ kao argument
+  void insert(iterator&, const T &);
+  void insert(iterator&&, const T &);
+  void remove(iterator&);
+  void remove(iterator&&);
 };
-
 
 //  DEFINICIJE METODA
-template<typename T>
-void List<T>::push_back(const T& el) {
+template <typename T> void List<T>::push_back(const T &el) {
 
-  Node* new_node_ptr = new Node{el};
-
-  if (empty()) {
-    head_ = tail_ = new_node_ptr;
-  } else {
-    tail_->next_ = new_node_ptr;
-    new_node_ptr->prev_ = tail_;
-    tail_ = new_node_ptr;
-  }
-
-  ++size_;
-
-};
-
-template<typename T>
-void List<T>::push_back(T&& el) {
-
-  Node* new_node_ptr = new Node{std::move(el)};
+  Node *new_node_ptr = new Node{el};
 
   if (empty()) {
     head_ = tail_ = new_node_ptr;
@@ -145,85 +148,104 @@ void List<T>::push_back(T&& el) {
   }
 
   ++size_;
-
 };
 
-template<typename T>
-void List<T>::push_front(const T& el) {
-  if (empty()) push_back(el); else {
-    Node* new_node_ptr = new Node{el, head_};
-		head_->prev_ = new_node_ptr;
-		head_ = new_node_ptr;
-    ++size_;
+template <typename T> void List<T>::push_back(T &&el) {
+
+  Node *new_node_ptr = new Node{std::move(el)};
+
+  if (empty()) {
+    head_ = tail_ = new_node_ptr;
+  } else {
+    tail_->next_ = new_node_ptr;
+    new_node_ptr->prev_ = tail_;
+    tail_ = new_node_ptr;
   }
 
+  ++size_;
 };
 
-template<typename T>
-void List<T>::push_front(T&& el) {
-  if (empty()) push_back(std::move(el)); else {
-    Node* new_node_ptr = new Node{std::move(el), head_};
-		head_->prev_ = new_node_ptr;
+template <typename T> void List<T>::push_front(const T &el) {
+  if (empty())
+    push_back(el);
+  else {
+    Node *new_node_ptr = new Node{el, head_};
+    head_->prev_ = new_node_ptr;
     head_ = new_node_ptr;
     ++size_;
   }
-
 };
 
-template<typename T>
-void List<T>::pop_back() {
-  if (empty()) return;
-  if (size_ == 1) return clear();
-  
-  Node* temp = tail_;
+template <typename T> void List<T>::push_front(T &&el) {
+  if (empty())
+    push_back(std::move(el));
+  else {
+    Node *new_node_ptr = new Node{std::move(el), head_};
+    head_->prev_ = new_node_ptr;
+    head_ = new_node_ptr;
+    ++size_;
+  }
+};
+
+template <typename T> void List<T>::pop_back() {
+  if (empty())
+    return;
+  if (size_ == 1)
+    return clear();
+
+  Node *temp = tail_;
 
   tail_ = tail_->prev_;
   tail_->next_ = nullptr;
 
-  temp->prev_ = nullptr; //vjerovatno bespotrebno jer se node taj brise ispod
+  temp->prev_ = nullptr; // vjerovatno bespotrebno jer se node taj brise ispod
   delete temp;
 };
 
-template<typename T>
-void List<T>::pop_front() {
-  if (empty()) return;
-  if (size_ == 1) return clear();
+template <typename T> void List<T>::pop_front() {
+  if (empty())
+    return;
+  if (size_ == 1)
+    return clear();
 
-  Node* temp = head_;
-  
+  Node *temp = head_;
+
   head_ = head_->next_;
-  head_-> prev_ = nullptr;
+  head_->prev_ = nullptr;
 
-  temp->next_ = nullptr; //isto vjerovatno bespotrebno jer se svakako ispod brise temp
+  temp->next_ =
+      nullptr; // isto vjerovatno bespotrebno jer se svakako ispod brise temp
   delete temp;
 };
 
-template <typename T>
-List<T>::List(const List& other) {
-  if (other.empty()) return;
-  for(Node* temp = other.head_; temp != nullptr; temp=temp->next_) push_back(temp->element_);
+template <typename T> List<T>::List(const List &other) {
+  if (other.empty())
+    return;
+  for (Node *temp = other.head_; temp != nullptr; temp = temp->next_)
+    push_back(temp->element_);
 };
 
 template <typename T>
-List<T>::List(List&& other) : head_{other.head_}, tail_{other.tail_}, size_{other.size_} {
+List<T>::List(List &&other)
+    : head_{other.head_}, tail_{other.tail_}, size_{other.size_} {
   other.head_ = nullptr;
   other.tail_ = nullptr;
   other.size_ = 0;
 };
 
-template <typename T>
-List<T>& List<T>::operator=(const List& other) {
-  if (this == &other) return *this;
+template <typename T> List<T> &List<T>::operator=(const List &other) {
+  if (this == &other)
+    return *this;
 
   clear();
-  for (Node* temp = other.head_; temp != nullptr; temp = temp->next_) push_back(temp->element_);
+  for (Node *temp = other.head_; temp != nullptr; temp = temp->next_)
+    push_back(temp->element_);
   return *this;
-
 };
 
-template <typename T>
-List<T>& List<T>::operator=(List&& other) {
-  if (this == &other) return *this;
+template <typename T> List<T> &List<T>::operator=(List &&other) {
+  if (this == &other)
+    return *this;
 
   clear();
 
@@ -238,10 +260,11 @@ List<T>& List<T>::operator=(List&& other) {
   return *this;
 };
 
-template<typename T>
+template <typename T>
 
 void List<T>::clear() {
-  if (size_ == 0) return;
+  if (size_ == 0)
+    return;
 
   auto temp = head_;
   while (temp != nullptr) {
@@ -254,59 +277,85 @@ void List<T>::clear() {
   tail_ = nullptr;
 }
 
-template <typename T>
-void List<T>::print() const {
+template <typename T> void List<T>::print() const {
 
-  for(Node* temp = head_; temp != nullptr; temp = temp->next_) {
+  for (Node *temp = head_; temp != nullptr; temp = temp->next_) {
     std::cout << temp->element_ << " ";
   }
   std::cout << std::endl;
 };
 
-template <typename T>
-void List<T>::insert(iterator& it, const T& el) {
+template <typename T> void List<T>::insert(iterator& it, const T &el) {
 
-	if (it == begin() || empty()) {
-		push_front(el);
-	} else 
+  if (it == begin()) {
+    push_front(el);
+  } else
 
-	if (it == end()) {
-		push_back(el);
-	} else {
+  if (it == end()) {
+    push_back(el);
+  } else {
 
-		Node* new_node = new Node{el}; 
-		new_node->next_ = it.p_;
-		new_node->prev_ = it.p_->prev_;
+    Node *new_node = new Node{el, it.p_, it.p_->prev_};
 
-		it.p_->prev_->next_ = new_node;
-		it.p_->prev_ = new_node;
+    it.p_->prev_->next_ = new_node;
+    it.p_->prev_ = new_node;
 
-		it.p_ = new_node;
+		it.pPrev_ = new_node;
 
-    size_++;
-	}
+    ++size_;
+  }
 };
 
-template <typename T>
-void List<T>::remove(iterator it) {
-  if (empty()) return;
+template <typename T> void List<T>::remove(iterator& it) {
 
-	if (it == begin()) {
-		pop_front();
-	} else 
+  if (it == begin()) {
+    pop_front();
+  } else
 
 	if (it == end()) {
-		pop_back();
-	} else {
+    pop_back();
+  } else {
 
+    it.p_->prev_->next_ = it.p_->next_;
+    it.p_->next_->prev_ = it.p_->prev_;
 
-		it.p_->prev_->next_ = it.p_->next_;
-		it.p_->next_->prev_ = it.p_->prev_;
+		it.pPrev_ = it.p_->prev_;
+    --size_;
+  }
+};
 
-		it.p_->prev_ = nullptr;
-		it.p_->next_ = nullptr;
-    it.p_ = nullptr;
+template <typename T> void List<T>::insert(iterator&& it, const T &el) {
 
-    size_--;
-	}
+  if (it == begin()) {
+    push_front(el);
+  } else
+
+  if (it == end()) {
+    push_back(el);
+  } else {
+
+    Node *new_node = new Node{el, it.p_, it.p_->prev_};
+
+    it.p_->prev_->next_ = new_node;
+    it.p_->prev_ = new_node;
+
+    ++size_;
+  }
+};
+
+template <typename T> void List<T>::remove(iterator&& it) {
+
+  if (it == begin()) {
+    pop_front();
+  } else
+
+	if (it == end()) {
+    pop_back();
+  } else {
+
+    it.p_->prev_->next_ = it.p_->next_;
+    it.p_->next_->prev_ = it.p_->prev_;
+
+    --size_;
+  }
 };
--- zad1/main.cpp
+++ zad1/main.cpp
@@ -6,20 +6,54 @@ int main ()  {
 	//Default konstruktor - prazna lista
 	List<int> lista;
 
-	//inserteri
+	//inserteri i iterator
+	lista.push_back(3);
+	lista.push_back(4);
+	lista.push_front(1);
 
-	List<int> lista2;
-	lista2.push_back(1);
-	lista2.push_front(0);
-	lista2.push_back(2);
-	lista2.push_back(3);
-	lista2.push_back(4);
-	lista2.push_back(5);
+	auto it = lista.begin();
+	lista.insert(it++, 0);
+	lista.insert(it, 2);
 
-	auto it2 = lista2.begin();
-	it2++;
-	lista2.insert(it2, 100);
-	lista2.print();
+	lista.print(); //print treba biti 0-4
+
+
+	std::cout << "Copy lista: ";
+
+	auto lista_cpy{lista}; // copy konstruktor
+	lista_cpy.print();
+
+	lista.clear(); // brisanje prve liste
+	lista = std::move(lista_cpy); //move operator iz copy liste nazad u og
+
+	std::cout << "Copy lista nakon move (treba biti prazna): ";
+	lista_cpy.print();
+	std::cout << "Originalna lista nakon move: ";
+	lista.print();
+
+
+	for (auto iter = lista.begin(); iter != lista.end(); ++iter) {
+		std::cout << *iter << std::endl;
+	}
+
+
+	auto iter = lista.end(); --iter; //iterator na kraj pa onda pomjeren na zadnji element
+	while(iter != lista.begin()) {
+		std::cout << *iter << std::endl;
+		--iter;
+	}
+
+
+	//Testiranje implementacije bidirekcionog iteratora
+	// Foreach petlja jedino radi ako je potpuno ispravna implementacija
+	// odgovarajuceg iteratora zajedno sa operatorima ==, !=
+	// u ovom slucaju usput testiramo i da li su iteratori pravilno 
+	// implementirani s obzirom da imaju 2 pointera, trenutni(p_) i prethodni(pPrev_)
+	std::cout << "For loop print: ";
+	for (auto& el : lista) {
+		std::cout << el << " ";
+	}
+	std::cout << std::endl;
+
   return 0;
-
 }
